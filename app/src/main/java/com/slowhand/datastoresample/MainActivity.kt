b8ca804019a3cb02930bc437e1fe500168350e14
Slowhand0309 <slowhand0309@gmail.com>
2021-09-23 16:05:28 +0900

    :heavy_plus_sign: Add Preferences DataStore sample

--- app/src/main/java/com/slowhand/datastoresample/MainActivity.kt
+++ app/src/main/java/com/slowhand/datastoresample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.slowhand.datastoresample
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -8,7 +10,20 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.slowhand.datastoresample.ui.theme.DataStoreSampleTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+val TEXT_KEY = stringPreferencesKey("example_text")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +36,19 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        GlobalScope.launch {
+            saveText(this@MainActivity, "sample")
+
+            val textFlow: Flow<String> = dataStore.data.map { p -> p[TEXT_KEY] ?: "" }
+            textFlow.collect { Log.d("DataStore", "text = $it") }
+        }
+    }
+}
+
+suspend fun saveText(context: Context, text: String) {
+    context.dataStore.edit { settings ->
+        settings[TEXT_KEY] = text
     }
 }
 
